dff444add57c22b63e5cbae66769ba75ce54b4cf
Sanjay Singh Shekhawat <shekhawatsanjay23@gmail.com>
2019-09-10 20:36:02 +0530

    update tests

--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
+# Test file
 import app
+
 
 def test_greet():
     assert app.greet_name() == 'Hi stranger!!!'
+
 
 def test_hello_world():
     assert app.hello_world() == '<H2> HELLO WORLD </H2>'
